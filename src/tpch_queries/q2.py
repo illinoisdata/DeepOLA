0cedefeb58d5b2093d93384659c1a4cfbc53c535
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2021-11-30 13:52:44 -0600

    Add example tpch_queries

---
+++ src/tpch_queries/q2.py
@@ -0,0 +1,27 @@
+from operations import *
+from query.query import Query
+
+q = Query()
+q.add_operation(name='table_lineitem',operation=TABLE(args={'table': 'lineitem'}))
+q.add_operation(name='table_customer',operation=TABLE(args={'table': 'customer'}))
+q.add_operation(name='table_orders',operation=TABLE(args={'table': 'orders'}))
+q.add_operation(name='where_c_mktsegment',operation=WHERE(args={'form':'DNF','predicates': [[{'left':'c_mktsegment','op':'==','right':'BUILDING'}]]}))
+q.add_operation(name='where_o_orderdate',operation=WHERE(args={'form':'DNF','predicates': [[{'left':'o_orderdate','op':'<','right':'1995-03-15'}]]}))
+q.add_operation(name='where_l_shipdate',operation=WHERE(args={'form':'DNF','predicates': [[{'left':'l_shipdate','op':'>','right':'1995-03-15'}]]}))
+q.add_operation(name='join_customer_order',operation=INNERJOIN(args={'left_on':['c_custkey'],'right_on':['o_custkey']}))
+q.add_operation(name='join_lineitem_order',operation=INNERJOIN(args={'left_on':['l_orderkey'],'right_on':['o_orderkey']}))
+q.add_operation(node_type="DM",name='groupby_operation',operation=GROUPBYAGG(args={'groupby_key':['l_orderkey','o_orderdate','o_shippriority'],'aggregates':[{'op':'sum','col':'l_extendedprice*(1-l_discount)','alias':'revenue'}]}))
+q.add_operation(node_type="DM",name='orderby_operation',operation=ORDERBY(args=[{'column':'revenue','order':'desc'},{'column':'o_orderdate'}]))
+q.add_operation(node_type="DM",name='limit_operation',operation=LIMIT(args={'k':20}))
+q.add_operation(output=True,name='select_operation',operation=SELECT(args={'columns':'*'}))
+
+q.add_edge('table_customer','where_c_mktsegment')
+q.add_edge('where_c_mktsegment','join_customer_order')
+q.add_edge('table_orders','join_customer_order')
+q.add_edge('table_lineitem','join_lineitem_order')
+q.add_edge('join_customer_order','join_lineitem_order')
+q.add_edge('join_lineitem_order','groupby_operation')
+q.add_edge('groupby_operation','orderby_operation')
+q.add_edge('orderby_operation','limit_operation')
+q.add_edge('limit_operation','select_operation')
+q.compile()
---
+++ src/tpch_queries/q3.py
@@ -0,0 +1,56 @@
+from operations import *
+from query.query import Query
+
+# SELECT
+#     l_returnflag,
+#     l_linestatus,
+#     sum(l_quantity) as sum_qty,
+#     sum(l_extendedprice) as sum_base_price,
+#     sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
+#     sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
+#     avg(l_quantity) as avg_qty,
+#     avg(l_extendedprice) as avg_price,
+#     avg(l_discount) as avg_disc,
+#     count(*) as count_order
+# FROM
+#     lineitem
+# WHERE
+#     l_shipdate <= date '1998-12-01' - interval '90' day
+# GROUP BY
+#     l_returnflag,
+#     l_linestatus
+# ORDER BY
+#     l_returnflag,
+#     l_linestatus;
+
+q = Query()
+q.add_operation(name='table_lineitem',operation=TABLE(args={'table': 'lineitem'}))
+q.add_operation(name='where_l_shipdate',operation=WHERE(args={'predicates': [[{'left':'l_shipdate','op':'<=','right':'1998-12-01'}]]}))
+q.add_operation(
+    node_type="DM",
+    name='groupby_operation',
+    operation=GROUPBYAGG(
+        args={
+            'groupby_key':['l_returnflag','l_linestatus'],
+            'aggregates':[
+                {'op':'sum','col':'l_quantity','alias':'sum_qty'},
+                {'op':'sum','col':'l_extendedprice','alias':'sum_base_price'},
+                {'op':'sum','col':'l_extendedprice * (1 - l_discount)','alias':'sum_disc_price'},
+                {'op':'sum','col':'l_extendedprice * (1 - l_discount) * (1 + l_tax)','alias':'sum_charge'},
+                {'op':'count','col':'l_quantity','alias':'count_qty'},
+                {'op':'count','col':'l_extendedprice','alias':'count_price'},
+                {'op':'sum','col':'l_discount','alias':'sum_disc'},
+                {'op':'count','col':'l_discount','alias':'count_disc'},
+                {'op':'count','col':'*','alias':'count_order'},
+            ]
+        }
+    )
+)
+q.add_operation(node_type="DM",name='orderby_operation',operation=ORDERBY(args=[{'column':'l_returnflag'},{'column':'l_linestatus'}]))
+q.add_operation(output=True,name='select_operation',operation=SELECT(args={'columns':'*'}))
+
+q.add_edge('table_lineitem','where_l_shipdate')
+q.add_edge('where_l_shipdate','groupby_operation')
+q.add_edge('groupby_operation','orderby_operation')
+q.add_edge('orderby_operation','select_operation')
+q.compile()
