08a194b89ac5ef2b1c37135747f56b2a431e7d61
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2022-02-22 13:08:53 -0600

    Move job to test workflow; Add more benchmarks

--- rust/runtime/benches/arithmetic.rs
+++ rust/runtime/benches/arithmetic.rs
@@ -4,8 +4,8 @@ use runtime::data::DataCell;
 // bench: find the `BENCH_SIZE` first terms of the arithmetic argument
 static BENCH_SIZE: i32 = 1000000;
 
-fn base_arithmetic(c: &mut Criterion) {
-    c.bench_function("Base Sum", |b| b.iter(||{
+fn base_arithmetic_sum_i32(c: &mut Criterion) {
+    c.bench_function("Base Sum i32", |b| b.iter(||{
         let mut data: Vec<i32> = vec![];
         for i in 0..BENCH_SIZE {
             data.push(i);
@@ -14,8 +14,18 @@ fn base_arithmetic(c: &mut Criterion) {
     }));
 }
 
-fn datacell_arithmetic(c: &mut Criterion) {
-    c.bench_function("DataCell Sum", |b| b.iter(||{
+fn base_arithmetic_sum_f64(c: &mut Criterion) {
+    c.bench_function("Base Sum f64", |b| b.iter(||{
+        let mut data: Vec<f64> = vec![];
+        for i in 0..BENCH_SIZE {
+            data.push(i.into());
+        }
+        data.iter().sum::<f64>()
+    }));
+}
+
+fn datacell_arithmetic_sum_i32(c: &mut Criterion) {
+    c.bench_function("DataCell Sum i32", |b| b.iter(||{
         let mut data: Vec<DataCell> = vec![];
         for i in 0..BENCH_SIZE {
             data.push(DataCell::Integer(i))
@@ -24,5 +34,43 @@ fn datacell_arithmetic(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches, datacell_arithmetic, base_arithmetic);
-criterion_main!(benches);
+fn datacell_arithmetic_sum_f64(c: &mut Criterion) {
+    c.bench_function("DataCell Sum f64", |b| b.iter(||{
+        let mut data: Vec<DataCell> = vec![];
+        for i in 0..BENCH_SIZE {
+            data.push(DataCell::Float(i.into()))
+        }
+        DataCell::sum(&data)
+    }));
+}
+
+
+fn base_arithmetic_mul_i32(c: &mut Criterion) {
+    c.bench_function("Base Mul i32", |b| b.iter(||{
+        let mut result = 1;
+        for i in 1..BENCH_SIZE {
+            result *= i;
+        }
+        result
+    }));
+}
+
+fn datacell_arithmetic_mul_i32(c: &mut Criterion) {
+    c.bench_function("DataCell Mul i32", |b| b.iter(||{
+        let mut result = DataCell::Integer(1);
+        for i in 1..BENCH_SIZE {
+            result = result * DataCell::Integer(i);
+        }
+        result
+    }));
+}
+
+criterion_group!(arithmetic_benches,
+    base_arithmetic_sum_i32,
+    datacell_arithmetic_sum_i32,
+    base_arithmetic_sum_f64,
+    datacell_arithmetic_sum_f64,
+    base_arithmetic_mul_i32,
+    datacell_arithmetic_mul_i32
+);
+criterion_main!(arithmetic_benches);
