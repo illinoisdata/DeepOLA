2212813b4389bfd1b1ec1a9d5f2a441d291dcea1
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2022-10-11 22:12:54 +0000

    Add queries and experiment scripts

---
+++ baselines/wanderjoin/2-setup-xdb.sh
@@ -0,0 +1,1 @@
+echo "Follow the README instructions from XDB Repository to setup; Make sure to set checkpoint_segments to a large value when starting postgres server"
--- baselines/wanderjoin/3-load-data.sh
+++ baselines/wanderjoin/3-load-data.sh
@@ -1,15 +1,26 @@
 if [ "$#" -ne 2 ]; then
-    echo "Usage: ./3-load-data.sh <scale: 10/50/100> <variation: data/skewed-data>"
-    exit
+   echo "Usage: ./3-load-data.sh <scale: 10/50/100> <variation: data/skewed-data>"
+   exit
 fi
+set -o xtrace
 SCALE=$1
 DATA_VARIATION=$2
+DB="wander_${SCALE}_var_${DATA_VARIATION}"
 FILE_PATH=/mnt/DeepOLA/resources/tpc-h/$DATA_VARIATION/scale=$SCALE/partition=1/tbl
-echo "COPY region FROM '$FILE_PATH/region.tbl' WITH delimiter '|' CSV;";
-echo "COPY nation FROM '$FILE_PATH/nation.tbl' WITH delimiter '|' CSV;";
-echo "COPY customer FROM '$FILE_PATH/customer.tbl' WITH delimiter '|' CSV;";
-echo "COPY supplier FROM '$FILE_PATH/supplier.tbl' WITH delimiter '|' CSV;";
-echo "COPY part FROM '$FILE_PATH/part.tbl' WITH delimiter '|' CSV;";
-echo "COPY partsupp FROM '$FILE_PATH/partsupp.tbl' WITH delimiter '|' CSV;";
-echo "COPY orders FROM '$FILE_PATH/orders.tbl' WITH delimiter '|' CSV;";
-echo "COPY lineitem FROM '$FILE_PATH/lineitem.tbl' WITH delimiter '|' CSV;";
+PSQL_BIN_DIR=/usr/local/pgsql/bin
+CREATEDB=$PSQL_BIN_DIR/createdb
+PSQL=$PSQL_BIN_DIR/psql
+
+$CREATEDB $DB
+$PSQL -d $DB -c "CREATE SCHEMA IF NOT EXISTS wanderjoin"
+$PSQL -d $DB -f "setup-queries/create-tables.sql"
+$PSQL -d $DB -c "COPY region FROM '$FILE_PATH/region.tbl' WITH delimiter '|' CSV;";
+$PSQL -d $DB -c "COPY nation FROM '$FILE_PATH/nation.tbl' WITH delimiter '|' CSV;";
+$PSQL -d $DB -c "COPY customer FROM '$FILE_PATH/customer.tbl' WITH delimiter '|' CSV;";
+$PSQL -d $DB -c "COPY supplier FROM '$FILE_PATH/supplier.tbl' WITH delimiter '|' CSV;";
+$PSQL -d $DB -c "COPY part FROM '$FILE_PATH/part.tbl' WITH delimiter '|' CSV;";
+$PSQL -d $DB -c "COPY partsupp FROM '$FILE_PATH/partsupp.tbl' WITH delimiter '|' CSV;";
+$PSQL -d $DB -c "COPY orders FROM '$FILE_PATH/orders.tbl' WITH delimiter '|' CSV;";
+$PSQL -d $DB -c "COPY lineitem FROM '$FILE_PATH/lineitem.tbl' WITH delimiter '|' CSV;";
+$PSQL -d $DB -f "setup-queries/create-index.sql"
+$PSQL -d $DB -f "setup-queries/scan-tables.sql"
