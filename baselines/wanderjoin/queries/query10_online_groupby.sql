c2e126d5737a820f76ab9a9be495feedfcd6747e
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2022-10-11 22:12:54 +0000

    Add queries and experiment scripts

--- baselines/wanderjoin/queries/query10_online_groupby.sql
+++ baselines/wanderjoin/queries/query10_online_groupby.sql
@@ -1,10 +1,8 @@
-\timing on
-SELECT SUM(l_extendedprice * (1 - l_discount))
+SELECT ONLINE c_mktsegment, SUM(l_extendedprice * (1 - l_discount))
 FROM customer, lineitem, orders, nation
 WHERE	c_custkey = o_custkey
 	AND	l_orderkey = o_orderkey
-	and o_orderdate >= date '1994-07-01'
 	AND l_returnflag = 'R'
 	AND c_nationkey = n_nationkey
-	AND c_acctbal > 3000;
-
+GROUP BY c_mktsegment
+WITHTIME 60000 CONFIDENCE 95 REPORTINTERVAL 1000;
