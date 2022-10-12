f582e455b49c5055bb4960ddb4b559d0317e1b75
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2022-10-12 17:43:49 +0000

    Fix Q2,Q17; Speedup Q19; Reorder columns Q10

--- deepola/wake/examples/tpch_polars/q19.rs
+++ deepola/wake/examples/tpch_polars/q19.rs
@@ -64,6 +64,23 @@ pub fn query(
     let lineitem_csvreader_node = build_reader_node("lineitem".into(), &tableinput, &table_columns);
     let part_csvreader_node = build_reader_node("part".into(), &tableinput, &table_columns);
 
+    // Predicate Pushdown
+    let lineitem_where_node = AppenderNode::<DataFrame, MapAppender>::new()
+        .appender(MapAppender::new(Box::new(|df: &DataFrame| {
+            let l_shipmode = df.column("l_shipmode").unwrap();
+            let l_shipinstruct = df.column("l_shipinstruct").unwrap();
+            const L_SHIPMODE_LIST: [&'static str; 2] = ["AIR", "AIR REG"];
+            let mask = l_shipinstruct.equal("DELIVER IN PERSON").unwrap()
+                & l_shipmode
+                    .utf8()
+                    .unwrap()
+                    .into_no_null_iter()
+                    .map(|v| L_SHIPMODE_LIST.contains(&v) as bool)
+                    .collect();
+            df.filter(&mask).unwrap()
+        })))
+        .build();
+
     // HASH JOIN Node
     let hash_join_node = HashJoinBuilder::new()
         .left_on(vec!["l_partkey".into()])
@@ -73,23 +90,16 @@ pub fn query(
     // WHERE Node
     let where_node = AppenderNode::<DataFrame, MapAppender>::new()
         .appender(MapAppender::new(Box::new(|df: &DataFrame| {
+            let p_size = df.column("p_size").unwrap();
             let p_brand = df.column("p_brand").unwrap();
             let p_container = df.column("p_container").unwrap();
-            let p_size = df.column("p_size").unwrap();
             let l_quantity = df.column("l_quantity").unwrap();
-            let l_shipmode = df.column("l_shipmode").unwrap();
-            let l_shipinstruct = df.column("l_shipinstruct").unwrap();
-
-            let common_mask = p_size.gt_eq(1i32).unwrap()
-                & l_shipinstruct.equal("DELIVER IN PERSON").unwrap()
-                & l_shipmode
-                    .utf8()
-                    .unwrap()
-                    .into_no_null_iter()
-                    .map(|v| vec!["AIR", "AIR REG"].contains(&v) as bool)
-                    .collect();
+            const P_CONTAINER_LIST_1: [&'static str; 4] = ["SM CASE", "SM BOX", "SM PACK", "SM PKG"];
+            const P_CONTAINER_LIST_2: [&'static str; 4] = ["MED BAG", "MED BOX", "MED PKG", "MED PACK"];
+            const P_CONTAINER_LIST_3: [&'static str; 4] = ["LG CASE", "LG BOX", "LG PACK", "LG PKG"];
 
             let mask1 = p_brand.equal("Brand#12").unwrap()
+                & p_size.gt_eq(1i32).unwrap()
                 & p_size.lt_eq(5i32).unwrap()
                 & l_quantity.gt_eq(1i32).unwrap()
                 & l_quantity.lt_eq(11i32).unwrap()
@@ -97,10 +107,11 @@ pub fn query(
                     .utf8()
                     .unwrap()
                     .into_no_null_iter()
-                    .map(|v| vec!["SM CASE", "SM BOX", "SM PACK", "SM PKG"].contains(&v) as bool)
+                    .map(|v| P_CONTAINER_LIST_1.contains(&v) as bool)
                     .collect();
 
             let mask2 = p_brand.equal("Brand#23").unwrap()
+                & p_size.gt_eq(1i32).unwrap()
                 & p_size.lt_eq(10i32).unwrap()
                 & l_quantity.gt_eq(10i32).unwrap()
                 & l_quantity.lt_eq(20i32).unwrap()
@@ -108,10 +119,11 @@ pub fn query(
                     .utf8()
                     .unwrap()
                     .into_no_null_iter()
-                    .map(|v| vec!["MED BAG", "MED BOX", "MED PKG", "MED PACK"].contains(&v) as bool)
+                    .map(|v| P_CONTAINER_LIST_2.contains(&v) as bool)
                     .collect();
 
             let mask3 = p_brand.equal("Brand#34").unwrap()
+                & p_size.gt_eq(1i32).unwrap()
                 & p_size.lt_eq(15i32).unwrap()
                 & l_quantity.gt_eq(20i32).unwrap()
                 & l_quantity.lt_eq(30i32).unwrap()
@@ -119,10 +131,10 @@ pub fn query(
                     .utf8()
                     .unwrap()
                     .into_no_null_iter()
-                    .map(|v| vec!["LG CASE", "LG BOX", "LG PACK", "LG PKG"].contains(&v) as bool)
+                    .map(|v| P_CONTAINER_LIST_3.contains(&v) as bool)
                     .collect();
 
-            let mask = common_mask & (mask1 | mask2 | mask3);
+            let mask = mask1 | mask2 | mask3;
             df.filter(&mask).unwrap()
         })))
         .build();
@@ -158,7 +170,8 @@ pub fn query(
         .build();
 
     // Connect nodes with subscription
-    hash_join_node.subscribe_to_node(&lineitem_csvreader_node, 0); // Left Node
+    lineitem_where_node.subscribe_to_node(&lineitem_csvreader_node, 0);
+    hash_join_node.subscribe_to_node(&lineitem_where_node, 0); // Left Node
     hash_join_node.subscribe_to_node(&part_csvreader_node, 1); // Right Node
     where_node.subscribe_to_node(&hash_join_node, 0);
     expression_node.subscribe_to_node(&where_node, 0);
@@ -173,6 +186,7 @@ pub fn query(
     service.add(expression_node);
     service.add(where_node);
     service.add(hash_join_node);
+    service.add(lineitem_where_node);
     service.add(part_csvreader_node);
     service.add(lineitem_csvreader_node);
     service
