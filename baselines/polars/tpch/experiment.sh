7bb98b9eb36cb439de4c0a3de2353e0b3221b105
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2023-07-21 21:20:08 -0700

    Add Polars as a baseline

--- baselines/polars/tpch/experiment.sh
+++ baselines/polars/tpch/experiment.sh
@@ -1,43 +1,45 @@
-if [ "$#" -ne 3 ]; then
-	echo "Usage: ./experiment.sh <scale-factor> <partition> <num-runs>"
-	exit
+#/!bin/bash
+
+int_handler() {
+    echo "Interrupted."
+    kill $PPID
+    exit 1
+}
+trap 'int_handler' INT
+
+if [ "$#" -ne 8 ]; then
+    echo "Expected 8 arguments, $# given."
+    echo "Usage: ./experiment.sh <data_dir> <output-dir> <scale-factor> <partition> <num-runs> <start-run> <start-query-id> <end-query-id>"
+    exit
 fi
 
-scale=$1
-partition=$2
-num_runs=$3
+data_dir=$1
+output_base_dir=$2
+scale=$3
+partition=$4
+num_runs=$5
+start_run=$6
+start_qdx=$7
+end_qdx=$8
 
 ## Set polars parameters to disable any additional logging
 export SCALE_FACTOR=$scale
 export PARTITION=$partition
-export DATA_BASE_DIR=/mnt/DeepOLA/
 export INCLUDE_IO=1
-#export SHOW_PLAN=0
-#export SHOW_RESULTS=0
-#export LOG_TIMINGS=0
-OUTPUT_DIR=outputs/scale=$scale/partition=$partition
+export SAVE_RESULTS=0
+OUTPUT_DIR=$output_base_dir/scale=$scale/partition=$partition
 mkdir -p $OUTPUT_DIR
-
-## Create virtualenv to execute polars queries
-make .venv
 
 ## Perform Benchmarking for Time
 echo "query,run,time,peak_mem" >> $OUTPUT_DIR/timings.csv
-for run in $(seq 0 $num_runs)
-do
-	echo "Experiment Run: $run"
-	for query in {1..23}
-	do
-		if [ $run -ne 0 ]; then
-			echo "Running Query: $query"
-			sync; echo 3 | sudo tee /proc/sys/vm/drop_caches
-			sleep 2
-			/usr/bin/time -f "$query,$run,%e,%M" ./.venv/bin/python -m polars_queries.q$query 2>>$OUTPUT_DIR/timings.csv
-		else
-			export SAVE_RESULTS=1
-			echo "Running Save Output Query: $query"
-			./.venv/bin/python -m polars_queries.q$query
-			export SAVE_RESULTS=0
-		fi
+
+# The experiment loop
+for ((qdx = ${start_qdx}; qdx <= ${end_qdx}; qdx++)); do
+    for ((j = ${start_run}; j <= ${num_runs}; j++)); do
+		echo ">>> ${qdx}, run = ${j}"
+		echo "Evicting Cache"
+		vmtouch -e ${data_dir}/scale=${scale}/partition=${partition}/parquet
+		sleep 2
+		/usr/bin/time --quiet -f "$qdx,$j,%e,%M" -a -o $OUTPUT_DIR/timings.csv python -m polars_queries.q${qdx}
 	done
 done
--- experiments.md
+++ experiments.md
@@ -28,6 +28,16 @@ docker build -t deepola-wake:sigmod2023 -f dockerfiles/wake.Dockerfile .
 
 Experiment results for each method will be saved under `results/<method>`.
 
+Polars (scale 100, partition 100, 10 runs, Q1-Q22):
+```bash
+DATA_DIR=/absolute/path/to/data # containing scale=100/partition=100/parquet
+docker run --rm \
+    -v ${DATA_DIR}:/dataset/tpch:rw \
+    -v `pwd`/results/polars:/outputs/polars \
+    --name polars deepola-wake:sigmod2023-polars \
+    bash experiment.sh /dataset/tpch /outputs/polars 100 100 10 1 1 22
+```
+
 Wake (scale 100, partition 100, 10 runs, Q1-Q22):
 ```bash
 DATA_DIR=/absolute/path/to/data  # containing scale=100/partition=100/[parquet|cleaned_parquet]
