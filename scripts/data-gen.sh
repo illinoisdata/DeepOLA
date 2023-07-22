b9c7a7c23ae0f4fed741a5883daa9ca6a052633b
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2023-07-21 21:53:14 -0700

    Add conversion to parquet

--- scripts/data-gen.sh
+++ scripts/data-gen.sh
@@ -2,12 +2,15 @@ if [ "$#" -ne 3 ]; then
 	echo "Usage: ./data-gen.sh <scale> <partition> <base-dir>. Example: ./data-gen.sh 1 10 $PWD/.."
 	exit
 fi
+
+cwd=$PWD
 scale=$1
 partition=$2
-BASE_DIR=$3
-export DSS_PATH=$BASE_DIR/resources/tpc-h/data/scale=$scale/partition=$partition/tbl
+output_dir=$3
+export DSS_PATH=$output_dir/scale=$scale/partition=$partition/tbl
 mkdir -p $DSS_PATH
 echo $DSS_PATH
+
 cd ../tpch-kit/dbgen/
 if [ $partition -eq 1 ];
 then
@@ -20,3 +23,7 @@ else
 			./dbgen -f -s $scale -S $chunk -C $partition -v
 		done
 fi
+
+echo "Output Directory: $DSS_PATH"
+cd $cwd
+python3 convert-to-parquet.py $DSS_PATH
